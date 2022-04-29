3062e3102bba152c0ea3547fca80716d6c27a407
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-04-29 07:01:26 +0500

    origin

---
+++ assets/js/functions.js
@@ -0,0 +1,16 @@
+$(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".slider-nav",
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 1.4,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: false,
+    infinite: false,
+    centerMode: true,
+    focusOnSelect: true,
+  });
