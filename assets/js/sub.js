92c5a271d3c8f2c362dfea17447fdbaff85b5f89
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-05-06 01:28:34 +0500

    origin

---
+++ assets/js/sub.js
@@ -0,0 +1,11 @@
+const handleDropdownMouseEnter =(selected)=>{
+    selected.querySelector('span img').src='../assets/images/svg/goldArrow.svg' 
+    selected.querySelector('span img').style.transform ='rotate(180deg)'
+    selected.querySelector('span img').style.top ='-2px'
+    
+}
+const handleDropdownMouseLeave =(selected)=>{
+    selected.querySelector('span img').src='../assets/images/svg/arrow.svg' 
+    selected.querySelector('span img').style.transform ='rotate(0deg)'
+    selected.querySelector('span img').style.top ='0'
+}
